51f66467ce5780607cc4adefcb49cbb70bde3636
sari2016 <sarithagovindaraju@gmail.com>
2026-01-10 20:38:51 -0600

    data-type

---
+++ shellscript/04-arrays.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MOVIES=("RRR" "PUSHPA" "DEVARA")
+echo "First Movie:: ${MOVIES[0]}"
+echo "Second Movie: ${MOVIES[1]}"
+echo "Third Movie: ${MOVIES[2]}"
+echo "All Movies are:: ${MOVIES[@]}"
+
